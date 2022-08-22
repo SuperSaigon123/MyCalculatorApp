15129c25ea35aa78849caa6c88176ef4fc727904
Sai Kovour <70875830+SuperSaigon123@users.noreply.github.com>
2022-08-22 12:55:31 -0500

    ferf

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mycalculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        public void findSum(View v) {
+            EditText number1ET = findViewById(R.id.firstNumber);
+            EditText number2ET = findViewById(R.id.secondNumber);
+            //TextView numberSumTV = findViewById(R.id.cal);
+
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int sum = num1 + num2;
+
+            //numberSumTV.setText("" + sum);
+        }
+
     }
 }
