2c9ff6a3667554c8b50c100440e0ddcb52aeeebe
Sai Kovour <70875830+SuperSaigon123@users.noreply.github.com>
2022-08-24 12:51:44 -0500

    8/24/2022
    calculator functionality has began working - still need a calculate button but should be doable by end of next class.

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -7,8 +7,7 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.view.View;
 import android.os.Bundle;
-
-import org.w3c.dom.Text;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,67 +15,81 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void doSomethingOnClick(View view){
+        Button calcButton = findViewById(R.id.calculateButton);
+        calcButton.setVisibility(View.GONE);
+
         Button addButton = findViewById(R.id.plusButton);
         Button subButton = findViewById(R.id.minusButton);
         Button multButton = findViewById(R.id.multiplyButton);
         Button divButton = findViewById(R.id.divideButton);
 
+        addButton.setOnClickListener(addOnClick);
+        subButton.setOnClickListener(subOnClick);
+        multButton.setOnClickListener(multOnClick);
+        divButton.setOnClickListener(divOnClick);
     }
 
-    public TextView findSum(View view) {
-        EditText number1ET = findViewById(R.id.firstNumber);
-        EditText number2ET = findViewById(R.id.secondNumber);
-        TextView numberSumTV = findViewById(R.id.answer);
+    private OnClickListener addOnClick = new OnClickListener(){
+        public void onClick(View v) {
+            EditText number1ET = findViewById(R.id.firstNumber);
+            EditText number2ET = findViewById(R.id.secondNumber);
+            TextView numberSumTV = findViewById(R.id.answer);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int sum = num1 + num2;
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int sum = num1 + num2;
 
-        numberSumTV.setText("" + sum);
-        return numberSumTV;
+            numberSumTV.setText("" + sum);
+        }
+
+    };
+
+    private OnClickListener subOnClick = new OnClickListener(){
+        public void onClick(View v) {
+            EditText number1ET = findViewById(R.id.firstNumber);
+            EditText number2ET = findViewById(R.id.secondNumber);
+            TextView numberDiffTV = findViewById(R.id.answer);
+
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int diff = num1 - num2;
+
+            numberDiffTV.setText("" + diff);
+        }
+
+    };
+
+    private OnClickListener multOnClick = new OnClickListener(){
+        public void onClick(View v) {
+            EditText number1ET = findViewById(R.id.firstNumber);
+            EditText number2ET = findViewById(R.id.secondNumber);
+            TextView numberProdTV = findViewById(R.id.answer);
+
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int prod = num1 * num2;
+
+            numberProdTV.setText("" + prod);
+        }
+
+    };
+
+    private OnClickListener divOnClick = new OnClickListener(){
+        public void onClick(View v) {
+            EditText number1ET = findViewById(R.id.firstNumber);
+            EditText number2ET = findViewById(R.id.secondNumber);
+            TextView numberQuoTV = findViewById(R.id.answer);
+
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int quo = num1 / num2;
+
+            numberQuoTV.setText("" + quo);
+        }
+    };
+
+    public TextView returnText(TextView t){
+        return t;
     }
-
-    public TextView findDifference(View view) {
-        EditText number1ET = findViewById(R.id.firstNumber);
-        EditText number2ET = findViewById(R.id.secondNumber);
-        TextView numberDiffTV = findViewById(R.id.answer);
-
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int diff = num1 - num2;
-
-        numberDiffTV.setText("" + diff);
-        return numberDiffTV;
-    }
-
-    public TextView findProduct(View view) {
-        EditText number1ET = findViewById(R.id.firstNumber);
-        EditText number2ET = findViewById(R.id.secondNumber);
-        TextView numberProdTV = findViewById(R.id.answer);
-
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int prod = num1 * num2;
-
-        numberProdTV.setText("" + prod);
-        return numberProdTV;
-    }
-
-    public TextView findQuotient(View view) {
-        EditText number1ET = findViewById(R.id.firstNumber);
-        EditText number2ET = findViewById(R.id.secondNumber);
-        TextView numberQuoTV = findViewById(R.id.answer);
-
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int quo = num1 / num2;
-
-        numberQuoTV.setText("" + quo);
-        return numberQuoTV;
-    }
-
-
 }
